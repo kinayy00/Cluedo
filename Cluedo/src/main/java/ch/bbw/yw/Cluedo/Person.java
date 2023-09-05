e0a441688d70057efbbab80ee64cb3dc75d77d06
Yanik <yanik.wisler@lernende.bbw.ch>
2023-09-05 11:15:19 +0200

    Person klasse erstellt

---
+++ Cluedo/src/main/java/ch/bbw/yw/Cluedo/Person.java
@@ -0,0 +1,119 @@
+package ch.bbw.yw.cluedo;
+
+public class Person {
+
+    private String name;
+
+    private String anrede;
+
+    private Color haarfarbe;
+
+    private int alter;
+
+    private String merkmal;
+
+ 
+
+    public enum Color {
+
+        RED, BLACK, BLONDE, BROWN, BLUE, GREEN,
+
+    }
+
+ 
+
+    public Person(String name, String anrede, Color haarfarbe, int alter, String merkmal) {
+
+        this.name = name;
+
+        this.anrede = anrede;
+
+        this.haarfarbe = haarfarbe;
+
+        this.alter = alter;
+
+        this.merkmal = merkmal;
+
+    }
+
+ 
+
+    public String getName() {
+
+        return name;
+
+    }
+
+ 
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+ 
+
+    public String getAnrede() {
+
+        return anrede;
+
+    }
+
+ 
+
+    public void setAnrede(String anrede) {
+
+        this.anrede = anrede;
+
+    }
+
+ 
+
+    public Color getHaarfarbe() {
+
+        return haarfarbe;
+
+    }
+
+ 
+
+    public void setHaarfarbe(Color haarfarbe) {
+
+        this.haarfarbe = haarfarbe;
+
+    }
+
+ 
+
+    public int getAlter() {
+
+        return alter;
+
+    }
+
+ 
+
+    public void setAlter(int alter) {
+
+        this.alter = alter;
+
+    }
+
+ 
+
+    public String getMerkmal() {
+
+        return merkmal;
+
+    }
+
+ 
+
+    public void setMerkmal(String merkmal) {
+
+        this.merkmal = merkmal;
+
+    }
+
+}
